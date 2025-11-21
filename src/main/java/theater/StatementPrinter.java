6d0bdf40a8e1bc5758c5af593d6bdf338d3c895a
3dr-zzZ <508874905@qq.com>
2025-11-21 17:54:45 -0500

    style fix

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -8,8 +8,14 @@ import java.util.Map;
  * This class generates a statement for a given invoice of performances.
  */
 public class StatementPrinter {
-    private Invoice invoice;
-    private Map<String, Play> plays;
+    private static final String TYPE_COMEDY = "comedy";
+    private static final String TYPE_TRAGEDY = "tragedy";
+
+    private final Invoice invoice;
+    private final Map<String, Play> plays;
+
+    private int lastTotalAmount;
+    private int lastTotalVolumeCredits;
 
     public StatementPrinter(Invoice invoice, Map<String, Play> plays) {
         this.invoice = invoice;
@@ -24,45 +30,75 @@ public class StatementPrinter {
         return plays;
     }
 
+    /**
+     * Returns the Play associated with the given play ID.
+     *
+     * @param playId the ID of the play to retrieve
+     * @return the Play object matching the given ID
+     * @null nothing
+     */
     public Play getPlay(String playId) {
         return plays.get(playId);
     }
 
-    public int getAmount(Performance p, Play play) {
-        return calculateAmount(p, play);
+    /**
+     * Computes the amount owed for a given performance and play.
+     *
+     * @param perf the performance instance
+     * @param play the play performed
+     * @return the amount in cents
+     * @null nothing
+     */
+    public int getAmount(Performance perf, Play play) {
+        return calculateAmount(perf, play);
     }
 
-    public int getVolumeCredits(Performance p, Play play) {
-        int credits = Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-        if ("comedy".equals(play.getType())) {
-            credits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+    /**
+     * Calculates the volume credits earned for a given performance.
+     *
+     * @param perf the performance instance
+     * @param play the associated play
+     * @return the number of volume credits earned
+     * @null nothing
+     */
+    public int getVolumeCredits(Performance perf, Play play) {
+        int credits = Math.max(perf.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        if (TYPE_COMEDY.equals(play.getType())) {
+            credits += perf.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
         }
         return credits;
     }
 
+    /**
+     * Formats a numeric amount (in cents) into a USD currency string.
+     *
+     * @param amount the amount in cents
+     * @return formatted USD currency string
+     * @null nothing
+     */
     public String usd(int amount) {
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
         return frmt.format(amount / (double) Constants.PERCENT_FACTOR);
     }
 
-    private int calculateAmount(Performance p, Play play) {
+    private int calculateAmount(Performance perf, Play play) {
         int thisAmount = 0;
         switch (play.getType()) {
-            case "tragedy":
+            case TYPE_TRAGEDY:
                 thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
-                if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                if (perf.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (p.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                            * (perf.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
-            case "comedy":
+            case TYPE_COMEDY:
                 thisAmount = Constants.COMEDY_BASE_AMOUNT;
-                if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                if (perf.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                            * (perf.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
                 }
-                thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * p.getAudience();
+                thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * perf.getAudience();
                 break;
             default:
                 throw new RuntimeException(String.format("unknown type: %s", play.getType()));
@@ -70,12 +106,12 @@ public class StatementPrinter {
         return thisAmount;
     }
 
-    private String formatLine(Play play, int thisAmount, Performance p, NumberFormat frmt) {
+    private String formatLine(Play play, int thisAmount, Performance perf, NumberFormat frmt) {
         return String.format(
                 "  %s: %s (%s seats)%n",
                 play.getName(),
                 frmt.format(thisAmount / Constants.PERCENT_FACTOR),
-                p.getAudience()
+                perf.getAudience()
         );
     }
 
@@ -103,12 +139,12 @@ public class StatementPrinter {
         for (Performance p : invoice.getPerformances()) {
             final Play play = plays.get(p.getPlayID());
 
-            int thisAmount = calculateAmount(p, play);
+            final int thisAmount = calculateAmount(p, play);
 
             // add volume credits
             volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
-            if ("comedy".equals(play.getType())) {
+            if (TYPE_COMEDY.equals(play.getType())) {
                 volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
             }
 
@@ -122,13 +158,23 @@ public class StatementPrinter {
         result.append(formatTotals(totalAmount, volumeCredits, frmt));
         return result.toString();
     }
-    private int lastTotalAmount = 0;
-    private int lastTotalVolumeCredits = 0;
 
+    /**
+     * Returns the most recently computed total amount from the last statement.
+     *
+     * @return the total amount in cents
+     * @null nothing
+     */
     public int getTotalAmount() {
         return lastTotalAmount;
     }
 
+    /**
+     * Returns the most recently computed total volume credits.
+     *
+     * @return total volume credits
+     * @null nothing
+     */
     public int getTotalVolumeCredits() {
         return lastTotalVolumeCredits;
     }
